623888e87222fb442576fed9ef9841ab288d9941
amanda-roberta <amanda.pereira.dias@escola.pr.gov.br>
2025-10-23 13:58:42 -0300

    khk

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,9 +6,11 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Em nossa sala, temos <span>${dados.Solteiro}</span> solteiros, enquanto que <span>${dados.Namorando}</span> estão namorando. Há <span>${dados.Enrolado}</span> enrolados. Por fim, temos <span>${dados.Fundo_poco}</span> no fundo do poço e <span>${dados.Casado}</span> casados.`
+    paragrafo.innerHTML = `Os três principais produtos do Valor Bruto da Produção (VBP) da agropecuária, são: Em primero, o Soja com <span>${dados.Soja}</span> bilhões, em segundo,a Carne Bovina com <span>${dados.Carne_Bovina}</span> bilhões. Em terceiro, o Milho com <span>${dados.Milho}</span> bilhões.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
+principais produtos do VBP da agropecuária (R$ bilhões)
+
 
 vizualizarInformacoesGlobais()
